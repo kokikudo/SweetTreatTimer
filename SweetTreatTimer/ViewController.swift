5ba78f4d5c0be66a049592345a1f987b2cc458b5
kokikudo <massanmesu@icloud.com>
2021-04-28 08:10:27 +0900

    Initial Commit

---
+++ SweetTreatTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SweetTreatTimer
+//
+//  Created by kudo koki on 2021/04/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
