c94eedb5c8eae9c55a927a643c4850b11101327c
kokikudo <massanmesu@icloud.com>
2021-04-28 09:30:51 +0900

    タイマー設定画面のレイアウトを作成

---
+++ SweetTreatTimer/TimerSettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TimerSettingViewController.swift
+//  SweetTreatTimer
+//
+//  Created by kudo koki on 2021/04/28.
+//
+
+import UIKit
+
+class TimerSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
